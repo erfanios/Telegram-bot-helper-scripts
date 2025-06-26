c9ada14f2ed7ee1c993ea8565ef9555aeda9a713
Erfan Karimi <erfankarimihbv@gmail.com>
2025-06-26 22:24:40 +0200

    Init

---
+++ bin/tel_send_message.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# SCRIPT: telegram_send_message.sh 
+# AUTHOR: ...
+# DATE: 2025-06-26T22:10:15
+# REV: 1.0
+# ARGUMENTS: [1:        ][2:		][3:		][4:        ]
+#
+# PURPOSE: Sends message to telegram api 
+#
+# set -x # Uncomment to debug
+# set -n # Uncomment to check script syntax without execution
+# set -e # Break on the first failure
+
+if [ -z "${t_api_key}" -o -z "${t_chat_id}" ]; then
+	echo "'t_chat_id' or 't_api_key' not defined in the environment" >&2
+	exit 1
+fi
+
+all="$@"
+
+curl -s -X POST https://api.telegram.org/bot${t_api_key}/sendMessage \
+	-d chat_id=${t_chat_id} \
+	-d text="${all:?'No message provided'}"
---
+++ deploy-to-bin.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SCRIPT: deploy-to-bin.sh 
+# AUTHOR: ...
+# DATE: 2025-06-26T22:22:59
+# REV: 1.0
+# ARGUMENTS: [1:        ][2:		][3:		][4:        ]
+#
+# PURPOSE: copy to local bin 
+#
+# set -x # Uncomment to debug
+# set -n # Uncomment to check script syntax without execution
+# set -e # Break on the first failure
+
+cp bin/* ~/bin/
